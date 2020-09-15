0deca0ec7e55be2a18ea34e290e4ac3adaae1286
SafaMagdy <safamagdy1999@gmail.com>
2020-09-15 22:14:57 +0200

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 
 @NgModule({
@@ -45,7 +46,11 @@ import { DishService } from './services/dish.service';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [
+    DishService,
+    PromotionService
+],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
